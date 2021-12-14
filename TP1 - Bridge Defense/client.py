fe9d9594b03b88eee0c2a4ab2d1f34a15649782c
Vando Diniz <vandocarlos2001@outlook.com>
2021-12-14 19:53:46 -0300

    Update client.py

--- TP1 - Bridge Defense/client.py
+++ TP1 - Bridge Defense/client.py
@@ -7,7 +7,7 @@ for c in range(0,4):
     dados = entrada.split(' ')
     SERVER = dados[0]
     PORT = int(dados[1])
-    SAG = '2019057195:12142021:713956ac462e3cc9736660c44697d3b6d91ffbe60ee2911114890582c2435f72+2019056890:12142021:d4ae8849f0d2f8ccf163b12a3fcf45908b61c8f2239f3806fe6292f3428a37ce+3933183216bb827a7cdca38687047dd1a191952b1afb1a01bcfd92ade29ae224'
+    SAG = dados[2]
     if PORT == 52221:
         RIVER = (SERVER, PORT)
     elif PORT == 52222:
@@ -21,5 +21,9 @@ for c in range(0,4):
         exit()
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.connect(RIVER)
+
+    def authreq(sag):
+
+
 
     #2019057195:12142021:713956ac462e3cc9736660c44697d3b6d91ffbe60ee2911114890582c2435f72+2019056890:12142021:d4ae8849f0d2f8ccf163b12a3fcf45908b61c8f2239f3806fe6292f3428a37ce+3933183216bb827a7cdca38687047dd1a191952b1afb1a01bcfd92ade29ae224
