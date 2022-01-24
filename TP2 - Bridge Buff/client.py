54e27b65c8535fd60ab5872626338c9711565048
Vando Diniz <vandocarlos2001@outlook.com>
2022-01-24 16:03:33 -0300

    .

--- TP2 - Bridge Buff/client.py
+++ TP2 - Bridge Buff/client.py
@@ -16,6 +16,62 @@ def entrada():
     info =[server,port,analyse]
     return info
 
+#REQUISITA UM JOGO ESPECIFICO
+def Analisa_Jogo(type):
+     #ENVIO
+    entrada = json.dumps({"type": "getcannons", "auth": SAG}).encode('utf-8')
+    client.sendto(entrada, ADDR) 
+
+    #RESPOSTA
+    saida = client.recv(bufferSize, 0)
+    resposta = json.loads(saida.decode('utf-8'))
+    print(resposta)
+    if type == 'auth':
+        SAGS.append = (resposta['auth'])
+    elif type == 'cannons':
+        CANNONS.append = (resposta['cannons'])
+
+# REQUISITA OS 100 MELHORES JOGOS DE UM TIPO
+def Analisa_Conjunto(type): 
+
+    #ENVIO
+    if type == 'sunk':
+        entrada = json.dumps({ "ranking": "sunk", "start": 1, "end": 100}).encode('utf-8')
+    elif type == 'escaped':
+        entrada = json.dumps({ "ranking": "escaped", "start": 1, "end": 100}).encode('utf-8')
+
+    client.sendto(entrada, ADDR) 
+
+    #RESPOSTA
+   
+    saida = client.recv(bufferSize, 0)
+    resposta = json.loads(saida.decode('utf-8'))
+    print(resposta)
+    game_ids = (resposta['game_ids'])
+    return game_ids
+
+# ANALISE 1 
+def myFunc(e):
+        return e['OCORRENCIAS']
+
+def Immortals(game_ids):
+    sags_ordenados = []
+    sags_usados = []
+    for id in game_ids:
+        Analisa_Jogo(id)
+
+    for auth in SAGS:
+        if auth not in sags_usados:
+            aux = {}
+            aux['SAG'] = auth
+            aux['OCORRENCIAS'] = SAGS.count(auth)
+            sags_ordenados.append(aux)
+            sags_usados.append(auth)
+    
+    sags_ordenados.sort(key=myFunc, reverse=True)
+    print(sags_ordenados)
+    return sags_ordenados
+
 #DEFININDO AS ESPECIFICAÇÕES DO SERVIDOR E PEGANDO AS INFORMAÇÕES DO TECLADO
 bufferSize = 4096   
 dados = entrada()
@@ -33,67 +89,24 @@ except:
     print('Erro a conectar com o servidor!')
     quit()
 
-# ANALISA UM ID INDIVIDUAL
-
 SAGS = []
 CANNONS = []
 
-def Analisa_Jogo(id, type):
-     #ENVIO
-    entrada = json.dumps({"type": "getcannons", "auth": SAG}).encode('utf-8')
-    client.sendto(entrada, ADDR) 
-
-    #RESPOSTA
-   
-    saida = client.recv(bufferSize, 0)
-    resposta = json.loads(saida.decode('utf-8'))
-    print(resposta)
-    if type == 'auth':
-        SAGS.append = (resposta['auth'])
-    if type == 'cannons':
-        CANNONS.append = (resposta['cannons'])
-
-# REQUISITA OS 100 MELHORES JOGOS 
-
-def Analisa_Conjunto(): 
-
-    #ENVIO
-    entrada = json.dumps({"type": "getcannons", "auth": SAG}).encode('utf-8')
-    client.sendto(entrada, ADDR) 
-
-    #RESPOSTA
-   
-    saida = client.recv(bufferSize, 0)
-    resposta = json.loads(saida.decode('utf-8'))
-    print(resposta)
-    game_ids = (resposta['game_ids'])
-    return game_ids
-
-
-# ANALISE 1 
-
-sags_ordenados = []
-sags_usados = []
-
-def myFunc(e):
-        return e['OCORRENCIAS']
-
-def Immortals(game_ids):
+if COMANDO == 1:
+    game_ids = Analisa_Conjunto('sunk')
     for id in game_ids:
-        Analisa_Jogo(id)
+        Analisa_Jogo('sunk')
+    sags_ordenados = Immortals()
 
-    for auth in SAGS:
-        if auth not in sags_usados:
-            aux = {}
-            aux['SAG'] = auth
-            aux['OCORRENCIAS'] = SAGS.count(auth)
-            sags_ordenados.append(aux)
-            sags_usados.append(auth)
-    
-    sags_ordenados.sort(key=myFunc, reverse=True)
-    print(sags_ordenados)
+if COMANDO == 2:
+    game_ids = Analisa_Conjunto('escaped')
+    for id in game_ids:
+        Analisa_Jogo('escaped')
+    #sags_ordenados = Immortals()
 
-# ANALISE 2
+
+
+
 
 
     
