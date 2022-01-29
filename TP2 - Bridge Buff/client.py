003dfec6c2ca0498753802e6145881d19c3fee90
Vando Diniz <vandocarlos2001@outlook.com>
2022-01-29 17:07:59 -0300

    .

--- TP2 - Bridge Buff/client.py
+++ TP2 - Bridge Buff/client.py
@@ -1,33 +1,22 @@
 import socket
 import json
+import sys
 
-def entrada():
-    while True:
-        entrada = input('Digite sua entrada: ')
-        dados = entrada.split(' ')
-        try:
-            dados2 = dados.split(':')
-            server = dados2[0]
-            port = int(dados2[1])
-            analyse = int(dados[1])
-            break
-        except:
-            print('Entrada inválida! Tente no seguinte formato: "SERVER:PORTA ANÁLISE"')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 
-    info =[server,port,analyse]
-    return info
+client.connect(('152.67.55.32', 8080))  
 
 #REQUISITA UM JOGO ESPECIFICO
 def Analisa_Jogo(type, id):
     #ENVIO
-    entrada = f"GET /api/game/{id} HTTP/1.1\r\nHost: 'http://152.67.55.32:8080/swagger'\r\n\r\n".encode('utf-8')
+    entrada = f"GET /api/game/{id} HTTP/1.1\r\nHost: '{url}'\r\n\r\n".encode('utf-8')
     client.send(entrada)  
 
     #RESPOSTA
-
     saida = client.recv(4096, 0)
     resposta = (saida.decode("utf-8"))
     
+    #MANIPULANDO A RESPOSTA
     cont = 0
     for letra in resposta:
         if letra == '{':
@@ -35,43 +24,46 @@ def Analisa_Jogo(type, id):
             break
         else:
             cont+=1
-
-    input()    
-    print(resposta)
     resposta = json.loads(resposta)
-    print(resposta)
    
+    #SALVANDO AS INFORMAÇÕES UTEIS
     if type == 'sunk':
         SAGS.append(resposta['game_stats']['auth'])
     elif type == 'escaped':
         CANNONS.append(resposta['cannons'])
-
+    
 # REQUISITA OS 100 MELHORES JOGOS DE UM TIPO
 def Analisa_Conjunto(type): 
 
     #ENVIO
     if type == 'sunk':
-        entrada = f"GET /api/rank/sunk?start=1&end=100 HTTP/1.1\r\nHost: 'http://152.67.55.32:8080/swagger'\r\n\r\n".encode('utf-8')
+        entrada = f"GET /api/rank/sunk?start=1&end=100 HTTP/1.1\r\nHost: '{url}'\r\n\r\n".encode('utf-8')
     elif type == 'escaped':
-        entrada = f"GET /api/rank/escaped?start=1&end=100 HTTP/1.1\r\nHost: 'http://152.67.55.32:8080/swagger'\r\n\r\n".encode('utf-8')
+        entrada = f"GET /api/rank/escaped?start=1&end=100 HTTP/1.1\r\nHost: '{url}'\r\n\r\n".encode('utf-8')
 
     client.send(entrada) 
-    print('enviou')
     #RESPOSTA
-   
     saida = client.recv(4096, 0)
-    print('recebeu')
     resposta = (saida.decode("utf-8"))
-    print(resposta)
-    input('teste')
+
+    #MANIPULANDO A RESPOSTA
+    cont = 0
+    for letra in resposta:
+        if letra == '{':
+            resposta = resposta[cont:]
+            break
+        else:
+            cont+=1
+    resposta = json.loads(resposta)
+
+    #SALVANDO AS INFORMAÇÕES UTEIS
     game_ids = (resposta['game_ids'])
-    print('vai retornar isso: ', game_ids)
     return game_ids
 
 
 def ordena(e):
         return e['OCORRENCIAS']
-
+    
 # ANALISE 1
 def Immortals():
     sags_ordenados = []
@@ -130,46 +122,50 @@ def Top_Meta():
     
     metas_ordenados.sort(key=ordena, reverse=True)
     return metas_ordenados
-        
-#DEFININDO AS ESPECIFICAÇÕES DO SERVIDOR E PEGANDO AS INFORMAÇÕES DO TECLADO
-COMANDO = int(input('Comando: '))
 
-#CONECTANDO COM O SERVIDOR
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.settimeout(0.5)
-try:
-    client.connect(('152.67.55.32', 8080))  
-except:
-    print('Erro a conectar com o servidor!')
-    quit()
+
+#DEFININDO AS ESPECIFICAÇÕES DO SERVIDOR E PEGANDO AS INFORMAÇÕES DO TECLADO
+dados = sys.argv
+url = dados[1]
+aux = url.split(':')
+IP = aux[0]
+PORT = int(aux[1])
+COMANDO = int(dados[2])
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 
 SAGS = []
 CANNONS = []
 
-Analisa_Jogo('sunk', 1)
+#CONECTANDO COM O SERVIDOR
+try:
+    client.connect((IP, PORT))
+except:
+    print('Erro a conectar com o servidor!')
+    quit()
+
+#ANALISES
 if COMANDO == 1:
-    print('entrou')
     game_ids = Analisa_Conjunto('sunk')
-    print('passou')
     for id in game_ids:
         Analisa_Jogo('sunk', id)
     sags_ordenados = Immortals()
-    print(sags_ordenados)
+
+    cont = 1
+    for ranking in sags_ordenados:
+        print(f'{cont}. {ranking}')
+        cont+=1
+    
 
 elif COMANDO == 2:
-    game_ids = Analisa_Conjunto('escaped')
+    game_ids = Analisa_Conjunto('sunk')
     for id in game_ids:
-        Analisa_Jogo('escaped')
+        Analisa_Jogo('sunk', id)
     metas_ordenados = Top_Meta()
-    print(metas_ordenados)
+
+    cont = 1
+    for ranking in metas_ordenados:
+        print(f'{cont}. {ranking}')
+        cont+=1
 
 client.close()
-
-
-
-
-
-
-
-    
-        
